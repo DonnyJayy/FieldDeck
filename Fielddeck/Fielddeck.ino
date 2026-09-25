7f3532b1d21b9835beb360fac62baa76197e472d
DonnyJayy <donnyjaydev@gmail.com>
2026-09-24 21:53:06 -0700

    Testing BNO055 function
    Tested the BNO055 for basic functions. Working good.

--- Fielddeck/Fielddeck.ino
+++ Fielddeck/Fielddeck.ino
@@ -1,11 +1,13 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
+#include <Adafruit_BNO055.h>
 
+// I2C pins
 #define SDA_PIN 15
 #define SCL_PIN 16
 
-Adafruit_BME280 bme;
+// Create BNO055 sensor
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29);
 
 void setup() {
   Serial.begin(115200);
@@ -14,36 +16,45 @@ void setup() {
   // Start I2C
   Wire.begin(SDA_PIN, SCL_PIN);
 
-  // Try the two common BME280 I2C addresses
-  if (!bme.begin(0x76, &Wire)) {
-    Serial.println("BME280 not found at 0x76, trying 0x77...");
+  Serial.println("Starting BNO055...");
 
-    if (!bme.begin(0x77, &Wire)) {
-      Serial.println("ERROR: BME280 not found!");
-      while (1) {
-        delay(1000);
-      }
+  if (!bno.begin()) {
+    Serial.println("ERROR: BNO055 not found!");
+    while (1) {
+      delay(1000);
     }
   }
 
-  Serial.println("BME280 connected!");
+  Serial.println("BNO055 connected!");
+
+  // Give the sensor time to initialize
+  delay(1000);
+
+  // Use external crystal for better accuracy if your module has one
+  bno.setExtCrystalUse(true);
+
+  Serial.println("BNO055 ready!");
   Serial.println();
 }
 
 void loop() {
-  Serial.print("Temperature: ");
-  Serial.print((bme.readTemperature() * 9.0 / 5.0) + 32.0);
-  Serial.println(" °F");
+  // Get orientation
+  sensors_event_t orientationData;
+  bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
 
-  Serial.print("Humidity: ");
-  Serial.print(bme.readHumidity());
-  Serial.println(" %");
+  Serial.print("Heading: ");
+  Serial.print(orientationData.orientation.x);
+  Serial.println("°");
 
-  Serial.print("Pressure: ");
-  Serial.print(bme.readPressure() / 100.0F);
-  Serial.println(" hPa");
+  Serial.print("Roll: ");
+  Serial.print(orientationData.orientation.y);
+  Serial.println("°");
+
+  Serial.print("Pitch: ");
+  Serial.print(orientationData.orientation.z);
+  Serial.println("°");
 
   Serial.println("--------------------");
 
-  delay(2000);
+  delay(500);
 }
